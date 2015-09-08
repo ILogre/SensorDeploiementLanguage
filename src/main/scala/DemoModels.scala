3bc8a2a58a58c393e8101d0b6dbf220add58e4ba
Cyril Cecchinel <cyrilcecchinel@lyoko.local>
2015-09-08 11:05:56 +0200

    demo: calculated field

--- src/main/scala/DemoModels.scala
+++ src/main/scala/DemoModels.scala
@@ -13,15 +13,16 @@ object DemoModels extends App{
   val SC_OpenClose = Observation("SC_OpenClose", timefield, Set(AtomicField("state", Discrete(Set(StringType("OPENED"), StringType("CLOSED"))))))
   val SC_Temperature = Observation("SC_Temperature", timefield, Set(AtomicField("v", Continuous(Some(IntegerType(Int.MinValue)), Some(IntegerType(Int.MaxValue))))))
 
+  val timefieldCalculated = Calculated("time", Continuous(Some(IntegerType(0)), Some(IntegerType(Int.MaxValue))), EOperator.Intersection, Set(SC_Temperature.time, SC_OpenClose.time))
+  val SC_Shame = Observation("SC_Shame",timefieldCalculated, Set(
+    AtomicField("temp", Continuous(Some(IntegerType(Int.MinValue)), Some(IntegerType(Int.MaxValue)))),
+    AtomicField("state", Discrete(Set(StringType("OPENED"), StringType("CLOSED"))))))
 
 
   val templiers = new Container("Templiers 1", EContainerType.Building, Set(new Container("Floor 4", EContainerType.Floor, Set(
     EventBased("door_SPARKS","when the stairs door is opened or closed", "localhost/data/door_sparks.json", SC_OpenClose),
     new Container("Office 444", EContainerType.Room, Set(Periodic("temp_444", 10, "localhost/data/temp_444.json", SC_Temperature)))
   ))))
-
-
-
 
 
 }
